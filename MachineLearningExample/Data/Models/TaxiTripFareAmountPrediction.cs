cb316a85f663df31c192809064ede86e2e21b25d
Matt Bacher <mattjb1991@sbcglobal.net>
2019-06-27 14:53:21 -0400

    Add project files.

---
+++ MachineLearningExample/Data/Models/TaxiTripFareAmountPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachineLearningExample.Data.Models
+{
+    public class TaxiTripFareAmountPrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
---
+++ MachineLearningExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MachineLearningExample
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "Models", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "Models", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Models", "Model.zip");
+        static void Main(string[] args)
+        {
+            MLContext context = new MLContext(seed: 0);
+
+            var model = Train(context, _trainDataPath);
+
+        }
+
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+
+        }
+    }
+}
